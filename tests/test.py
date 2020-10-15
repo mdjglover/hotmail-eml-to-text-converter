d214cced64f6626d8445242337bc6cb6c64d288f
Mike Glover <michaeldjglover@gmail.com>
2020-10-15 20:19:17 +0100

    added test for single email chain

--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,24 @@ from hotmail_eml_to_txt_converter.parser.HotmailEMLChainParser import HotmailEML
 
 
 class test_HotmailEMLChainParser(unittest.TestCase):
-    def test_parser(self):
+    def test_parser_one_email(self):
         emails = []
         correspondents = {}
-        with open("Z:/dev/projects/eml-to-text-converter/tests/test-source/test.eml") as f:
+        with open("Z:/dev/projects/eml-to-text-converter/tests/test-source/test_one_email.eml") as f:
+            parser = HotmailEMLChainParser(f)
+            emails = parser.get_emails()
+            correspondents = parser.get_correspondents()
+        
+        self.assertEqual(len(emails), 1)
+        self.assertIn("person_one@hotmail.com", correspondents)
+        self.assertIn("person_two@hotmail.com", correspondents)
+        self.assertIn("Cyril", emails[0].body)
+        self.assertEqual("18-11-50", emails[0].time)
+
+    def test_parser_two_emails(self):
+        emails = []
+        correspondents = {}
+        with open("Z:/dev/projects/eml-to-text-converter/tests/test-source/test_two_emails.eml") as f:
             parser = HotmailEMLChainParser(f)
             emails = parser.get_emails()
             correspondents = parser.get_correspondents()
